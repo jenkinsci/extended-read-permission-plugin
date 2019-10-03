18b47628defc5706a4baf2220e25969af6a3879b
Raihaan Shouhell <raihaan.shouhell@autodesk.com>
2019-10-03 16:03:37 +0800

    Restrict class

--- src/main/java/hudson/plugins/extendedread/ExtendedReadPermissionEnabler.java
+++ src/main/java/hudson/plugins/extendedread/ExtendedReadPermissionEnabler.java
@@ -1,11 +1,15 @@
 package hudson.plugins.extendedread;
 
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
+
 import hudson.Extension;
 import hudson.init.InitMilestone;
 import hudson.init.Initializer;
 import hudson.model.Item;
 
 @Extension
+@Restricted(NoExternalUse.class)
 public class ExtendedReadPermissionEnabler {
     @Initializer(after = InitMilestone.STARTED)
     public static void enableExtendedReadPermission() {
