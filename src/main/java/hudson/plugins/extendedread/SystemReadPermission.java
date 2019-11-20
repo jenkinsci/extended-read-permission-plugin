8ea5e1f0616432f32e2f5566712e82762d6f5f36
Tim Jacomb <timjacomb1+github@gmail.com>
2019-11-20 18:37:17 +0000

    Make as beta

--- src/main/java/hudson/plugins/extendedread/SystemReadPermission.java
+++ src/main/java/hudson/plugins/extendedread/SystemReadPermission.java
@@ -6,9 +6,12 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import jenkins.model.Jenkins;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.Beta;
 
 import static jenkins.model.Jenkins.ADMINISTER;
 
+@Restricted(Beta.class)
 public class SystemReadPermission {
 
     private static final Logger LOGGER = Logger.getLogger(SystemReadPermission.class.getName());
