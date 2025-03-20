df4c3634e5b9444bcfeb63ad6dfb8678ec21cb1c
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-03-20 16:27:56 +0100

    Migrate tests to JUnit5
    * Migrate annotations and imports
    * Migrate assertions
    * Remove public visibility for test classes and methods
    * Minor code cleanup

--- src/test/java/hudson/plugins/extendedread/SystemReadPermissionEnablerTest.java
+++ src/test/java/hudson/plugins/extendedread/SystemReadPermissionEnablerTest.java
@@ -3,27 +3,25 @@ package hudson.plugins.extendedread;
 import hudson.security.Permission;
 import hudson.util.VersionNumber;
 import jenkins.model.Jenkins;
-import org.junit.Rule;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.jvnet.hudson.test.JenkinsRule;
+import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
 
 import static java.util.Objects.requireNonNull;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assume.assumeThat;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
-public class SystemReadPermissionEnablerTest {
-    @Rule
-    public JenkinsRule j =  new JenkinsRule();
+@WithJenkins
+class SystemReadPermissionEnablerTest {
 
     @Test
-    public void testSystemReadPermissionEnabled() {
+    void testSystemReadPermissionEnabled(JenkinsRule j) {
         VersionNumber version = requireNonNull(Jenkins.getVersion());
-        assumeThat("This test requires Jenkins core version 2.221 or newer", version, greaterThan(new VersionNumber("2.221")));
-        
+        assumeTrue(version.isNewerThan(new VersionNumber("2.221")), "This test requires Jenkins core version 2.221 or newer");
+
         Permission systemRead = SystemReadPermission.SYSTEM_READ;
-        
+
         // let's be safe and validate we aren't using the administer fallback
         assertThat(systemRead.name, is("SystemRead"));
         assertThat(systemRead.getEnabled(), is(true));
